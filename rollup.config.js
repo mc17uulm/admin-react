dcd2bb252cd0f92586847036ee4abcc729e299cf
mc17uulm <marco.combosch@uni-ulm.de>
2022-05-12 11:38:09 +0200

    fresh rebuild

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
@@ -32,7 +32,7 @@ export default [
         external: ["react", "react-dom", "styled-components"]
     },
     {
-        input: "dist/esm/types.d.ts",
+        input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
     },
