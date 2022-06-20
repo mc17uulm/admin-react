52312418282dc1e76803ba7c3846fbb20e75c391
Marco Combosch <marco@combosch.de>
2022-06-20 22:45:15 +0200

    added nextjs for examples; fixed minor sidebar bug

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 // Components
-export { Generic, type GenericProps } from './components/Generic';
-export { Tab, type TabPane } from './components/Tab';
-export { Row, Col } from './components/Row';
-export { Button } from './components/Button';
-export { Icon } from './components/Icon';
-export { Card, CardHeader, CardBody } from './components/Card';
-export { Content } from './components/Content';
-export { Footer, FooterVersion } from './components/Footer';
-export { Header, Breadcrumb } from './components/Header';
-export { Navbar, NavbarNav, NavItem, NavLink } from './components/Navbar';
-export { Modal, ModalHeader, ModalTitle, ModalBody, ModalFooter } from './components/Modal';
+export { Generic, type GenericProps } from './src/components/Generic';
+export { Tab, type TabPane } from './src/components/Tab';
+export { Row, Col } from './src/components/Row';
+export { Button } from './src/components/Button';
+export { Icon } from './src/components/Icon';
+export { Card, CardHeader, CardBody } from './src/components/Card';
+export { Content } from './src/components/Content';
+export { Footer, FooterVersion } from './src/components/Footer';
+export { Header, Breadcrumb } from './src/components/Header';
+export { Navbar, NavbarNav, NavItem, NavLink } from './src/components/Navbar';
+export { Modal, ModalHeader, ModalTitle, ModalBody, ModalFooter } from './src/components/Modal';
 export { 
     type InputType, 
     type InputProps, 
@@ -20,7 +20,7 @@ export {
     InputAppend, 
     InputGroupText, 
     Checkbox
-} from './components/Input';
+} from './src/components/Input';
 export {
     type SidebarLink,
     Sidebar,
@@ -31,7 +31,7 @@ export {
     SidebarSearchPanel,
     SidebarUserPanel,
     SidebarHeader
-} from './elements/Sidebar';
+} from './src/elements/Sidebar';
 export {
     Table,
     TableHead,
@@ -39,20 +39,20 @@ export {
     TableBody,
     TableColumn,
     TableRow
-} from './components/Table';
+} from './src/components/Table';
 
 // Elements
-export { LoginBox, LoginLogo, LoginBoxMsg } from './elements/Login';
-export { type Badge, MenuDropdown, MenuItem } from './elements/Menu';
-export { SearchBlock } from './elements/SearchBlock';
+export { LoginBox, LoginLogo, LoginBoxMsg } from './src/elements/Login';
+export { type Badge, MenuDropdown, MenuItem } from './src/elements/Menu';
+export { SearchBlock } from './src/elements/SearchBlock';
 
 // Types
 export type { Color, Size } from './types';
 
 // Hooks
-export { useClassName } from './hooks/useClassName';
-export { useDefault } from './hooks/useDefault';
-export { useModal } from './hooks/useModal';
-export { useOnOutsideClick } from './hooks/useOnOutsideClick'; 
-export { useBodyClass, useBodyStyle } from './hooks/useBody';
-export { useSidebar } from './hooks/useSidebar';
+export { useClassName } from './src/hooks/useClassName';
+export { useDefault } from './src/hooks/useDefault';
+export { useModal } from './src/hooks/useModal';
+export { useOnOutsideClick } from './src/hooks/useOnOutsideClick';
+export { useBodyClass, useBodyStyle } from './src/hooks/useBody';
+export { useSidebar } from './src/hooks/useSidebar';
